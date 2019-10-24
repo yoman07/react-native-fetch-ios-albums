5d2cb5dba43e2bba2cd458925b2caad051225c33
Roman Barzyczak <roman.barzyczak@gmail.com>
2019-10-24 14:56:24 +0200

    forked project

---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { ReactNativeFetchAlbums } = NativeModules;
+
+export default ReactNativeFetchAlbums;
---
+++ ios/ReactNativeFetchAlbums.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface ReactNativeFetchAlbums : NSObject <RCTBridgeModule>
+
+@end
